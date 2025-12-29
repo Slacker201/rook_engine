6463ba44bbec36d22bbe96886b3d69c3d8860594
Slacker201 <slacker1234567809@gmail.com>
2025-12-29 11:59:48 -0600

    added trump to play_turn and winner selection logic

--- src/engine/ingame.rs
+++ src/engine/ingame.rs
@@ -8,12 +8,13 @@ impl RookEngine {
         let mut pot = [Card::Null; 4];
         let mut idx = 0;
         for player in players {
-            let card_played = player.play_turn(pot);
+            let card_played = player.play_turn(trump, pot);
             let card = player.get_and_remove(card_played);
             pot[idx] = card;
             idx += 1;
         }
 
+        let winner = players[Self::get_index_of_winner(pot)];
 
     }
 
@@ -22,5 +23,9 @@ impl RookEngine {
         let mut slice_1 = players.split_off(turn.to_idx());
         slice_1.extend(players);
         slice_1.try_into().unwrap()
+    }
+
+    fn get_index_of_winner(trump: CardSuit, pot: [Card; 4]) -> usize {
+        let first_card_suit = pot[0].suit();
     }
 }
