fbf50a1445129f0154616a7d65d68209a83fb6ab
Slacker201 <slacker1234567809@gmail.com>
2025-12-29 11:51:37 -0600

    added arrange function to arrange players according to who players first

--- src/engine/ingame.rs
+++ src/engine/ingame.rs
@@ -1,9 +1,16 @@
-use crate::engine::{RookEngine, card::CardSuit, engine_state::Turn};
+use crate::engine::{RookEngine, card::{Card, CardSuit}, engine_player_state::EnginePlayerState, engine_state::Turn};
 
 
 
 impl RookEngine {
     pub fn play_trick(&mut self, trump: CardSuit, turn: Turn) {
 
+    }
+
+    fn get_players_arranged_to_turn(&mut self, turn: Turn) -> [&mut EnginePlayerState; 4] {
+        let mut players: Vec<&mut EnginePlayerState> = self.players.iter_mut().collect();
+        let mut slice_1 = players.split_off(turn.to_idx());
+        slice_1.extend(players);
+        slice_1.try_into().unwrap()
     }
 }
