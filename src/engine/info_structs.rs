b79a70ae3b43e03a5c3076c4d5255fc9765f9dad
Slacker201 <slacker1234567809@gmail.com>
2025-12-30 12:04:54 -0600

    Add missing derive attributes for PostBidInformation and PostTrickInformation structs

--- src/engine/info_structs.rs
+++ src/engine/info_structs.rs
@@ -3,6 +3,7 @@ use crate::engine::{
     engine_state::Turn,
 };
 
+#[derive(Debug, Clone, Copy)]
 pub struct PostBidInformation {
     pub bid_winner: Turn,
     pub your_turn: Turn,
@@ -10,6 +11,7 @@ pub struct PostBidInformation {
     pub trump_suit: CardSuit,
 }
 
+#[derive(Debug, Clone, Copy)]
 pub struct PostTrickInformation {
     pub trick_winner: Turn,
     pub your_turn: Turn,
