a04ea3f535f52c67b98c9ef83d053cd6ca15e25b
Slacker201 <slacker1234567809@gmail.com>
2025-12-30 11:18:41 -0600

    added game_won logic

--- src/engine/game_won.rs
+++ src/engine/game_won.rs
@@ -1,10 +1,48 @@
-use crate::engine::RookEngine;
 
 
+use crate::engine::{RookEngine, engine_state::Turn};
+
+#[derive(Debug)]
+enum Team {
+    TeamOne,
+    TeamTwo,
+}
 
 
 impl RookEngine {
     pub fn game_won(&mut self) {
-        
+        // find winner
+        let bidder_score = {
+            match self.get_bid_team() {
+                Team::TeamOne => {
+                    self.players[0].score() + self.players[2].score()
+                },
+                Team::TeamTwo => {
+                    self.players[1].score() + self.players[3].score()
+                },
+            }
+        };
+        if bidder_score >= self.bid {
+            println!("Winner is {:?}", self.get_bid_team())
+        } else {
+
+            println!("Winner is {:?}", self.get_bid_team().swap())
+        }
+    }
+
+    fn get_bid_team(&self) -> Team {
+        match self.bid_winner {
+            Turn::One | Turn::Three => Team::TeamOne,
+            Turn::Two | Turn::Four => Team::TeamTwo,
+        }
+    }
+}
+
+impl Team {
+    fn swap(self) -> Self {
+        match self {
+            Team::TeamOne => Team::TeamTwo,
+            Team::TeamTwo => Team::TeamOne,
+        }
     }
 }
