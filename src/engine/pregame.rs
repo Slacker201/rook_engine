0d93a60bca563c3162a62ee9d9a850b06fedddfb
Slacker201 <slacker1234567809@gmail.com>
2025-12-31 11:20:37 -0600

    refactor(engine/pregame): update card initialization logic
    - Replace `players_have_sufficient_points` with `should_reshuffle`.
    - Use new method from `Deck` struct to get player cards.
    - Simplify hand distribution and reshuffling logic.

--- src/engine/pregame.rs
+++ src/engine/pregame.rs
@@ -1,30 +1,24 @@
-use rand::seq::SliceRandom;
-
-use crate::engine::{RookEngine, card::Card, engine_state::EngineState};
+use crate::engine::{RookEngine, card::{deck::{CardScoring, Deck}}, engine_state::EngineState};
 
 impl RookEngine {
     pub fn init_pregame(&mut self) {
-        let mut players_have_sufficient_points = false;
-        while !players_have_sufficient_points {
+        let mut should_reshuffle = true;
+        while should_reshuffle {
+            should_reshuffle = false;
             println!("Shuffling Cards");
-            players_have_sufficient_points = true;
-            let mut deck = Card::new_deck();
-            deck.shuffle(&mut self.rng);
-            // dole out cards
+            let deck = Deck::new();
+            let cards = deck.get_player_cards();
 
             // pick 10 cards for each player
             let mut i = 0;
-
-            for player in &mut self.players {
-                player.set_hand(deck[i..i + 10].try_into().unwrap());
-                if !player.player_has_sufficient_points() {
-                    players_have_sufficient_points = false;
-                    println!("A player has insufficient points :(");
-                    break;
+            for hand in cards.0 {
+                self.players[i].set_hand(hand);
+                if hand.points() == 15 {
+                    should_reshuffle = self.players[i].should_reshuffle() || should_reshuffle;
                 }
-                i += 10;
+                i += 1;
             }
-            self.state = EngineState::Bid(deck[40..45].try_into().unwrap())
+            self.state = EngineState::Bid(cards.1)
         }
     }
 }
