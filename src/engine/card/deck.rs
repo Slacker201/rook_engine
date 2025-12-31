19e59f67bb2e8ea89c278207f05381bf8aff9724
Slacker201 <slacker1234567809@gmail.com>
2025-12-31 10:52:55 -0600

    Refactor Deck to implement CardScoring trait and enhance player card reshuffling logic

--- src/engine/card/deck.rs
+++ src/engine/card/deck.rs
@@ -8,6 +8,10 @@ pub struct Deck {
     cards: [Card; 45]
 }
 
+trait CardScoring {
+    fn points(&self) -> u32;
+}
+
 impl Deck {
     pub fn new() -> Self {
         let cards = Card::new_deck();
@@ -18,17 +22,47 @@ impl Deck {
     }
     pub fn get_player_cards(mut self) -> ([[Card; 10]; 4], [Card; 5]) {
         let mut rng = rand::rng();
-        self.shuffle(&mut rng);
-        let players = [
-            self.cards[0..10].try_into().unwrap(),
-            self.cards[10..20].try_into().unwrap(),
-            self.cards[20..30].try_into().unwrap(),
-            self.cards[30..40].try_into().unwrap(),
-        ];
+        let players = loop {
+            // shuffle the deck
+            self.shuffle(&mut rng);
+
+            // generate the players cards
+            let players: [[Card; 10]; 4] = [
+                self.cards[0..10].try_into().unwrap(),
+                self.cards[10..20].try_into().unwrap(),
+                self.cards[20..30].try_into().unwrap(),
+                self.cards[30..40].try_into().unwrap(),
+            ];
+
+            // check if we should reshuffle
+            let should_reshuffle = players.iter().all(|vector| {
+                let score = vector.points();
+                if score >= 15 {
+                    return false
+                } else if score < 15 {
+                    return true
+                } else {
+                    false
+                }
+            });
+            if !should_reshuffle {
+                break players;
+            }
+        };
         let nest = self.cards[40..45]
             .try_into()
             .expect("Somehow the atoms of the universe conspired against you!");
         (players, nest)
+    }
+}
+
+impl CardScoring for [Card] {
+    fn points(&self) -> u32 {
+        let mut score = 0;
+        for card in self {
+            score += card.points()
+        }
+        score
     }
 }
 
